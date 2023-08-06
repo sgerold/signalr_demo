ab7e9f0bbcfd5e664b8402ea6345dd5c93d3502a
Scott Gerold <scott@futuresystemsinc.com>
2023-08-06 08:30:04 -0400

    Initial Test

---
+++ signalr_demo/Hubs/TaskListHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+
+namespace signalr_demo.Hubs
+{
+    public class TaskListHub : Hub
+    {
+        public async Task AddTask(string task)
+        {
+            await Clients.All.SendAsync("TaskAdded", task);
+        }
+
+        public async Task RemoveTask(string task)
+        {
+            await Clients.All.SendAsync("TaskRemoved", task);
+        }
+    }
+}
